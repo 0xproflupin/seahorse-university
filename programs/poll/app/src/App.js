04abf72d37161e2f068af444041a18119f9deab7
0xproflupin <mac.anvit@gmail.com>
2023-06-14 18:31:19 +0530

    durable nonces

--- programs/poll/app/src/App.js
+++ programs/poll/app/src/App.js
@@ -2,6 +2,7 @@ import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import {
   ConnectionProvider,
   useWallet,
+  useConnection,
   useAnchorWallet,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
@@ -16,9 +17,19 @@ import "@solana/wallet-adapter-react-ui/styles.css";
 import {
   clusterApiUrl,
   PublicKey,
+  Transaction,
+  Keypair,
+  SystemProgram,
+  NonceAccount,
+  sendAndConfirmRawTransaction,
+  LAMPORTS_PER_SOL,
+  NONCE_ACCOUNT_LENGTH,
 } from '@solana/web3.js';
-import React, { useEffect } from 'react';
+import { createClient } from '@supabase/supabase-js'
+import React from 'react';
 import idl from "./idl.json";
+import * as bs58 from "bs58";
+import { v4 as uuidv4 } from 'uuid';
 
 const programID = new PublicKey("5MgjVvaSLj6zmxuYhSST1M4LBiXoiSMrJPDZTRPQoiw8");
 const network =
@@ -26,7 +37,17 @@ const network =
 const opts = {
   preflightCommitment: "processed",
 };
-  
+const supabase = createClient('https://jduhbkqikqrairwvksmu.supabase.co', process.env.REACT_APP_SUPABASE_KEY)
+
+const nonceAuthKP = Keypair.fromSecretKey(
+  Uint8Array.from([
+    152,85,82,57,118,154,160,122,133,227,18,227,56,109,149,232,186,151,74,169,
+    228,10,31,157,102,58,130,12,159,2,69,144,114,213,143,146,242,145,207,157,
+    117,231,235,172,43,117,83,17,198,234,122,59,237,145,56,227,62,131,168,110,
+    109,57,5,40
+  ])
+);
+
 export const getProvider = (wallet) => {
   /* create the provider and return it to the caller */
   /* network set to local network for now */
@@ -70,8 +91,13 @@ const Context = ({ children }) => {
 };
 
 const Content = () => {
-  const { publicKey } = useWallet();
+  const { publicKey, signTransaction } = useWallet();
+  const { connection } = useConnection();
   const [poll, setPoll] = useState(null);
+  const [enteredAddress, setEnteredAddress] = useState('');
+  const [enteredNumber, setEnteredNumber] = useState(0);
+  const [noncePubKeys, setNoncePubKeys] = useState([]);
+  const [nonces, setNonces] = useState([]);
   const [votes, setVotes] = useState({
     solana: 0,
     ethereum: 0,
@@ -81,42 +107,62 @@ const Content = () => {
   const program = getProgram(wallet);
   console.log(`Poll program: ${program.programId}`);
   console.log(`Connected wallet: ${wallet?.publicKey}`);
-  console.log(`Poll account: ${poll?.publicKey}`);
+  console.log(`Poll account: ${poll}`);
   console.log(`Current poll status: Ethereum=${votes.ethereum} | Solana=${votes.solana} | Polygon=${votes.polygon}`);
   console.log(``);
 
-  useEffect(() => {
-    const fetchPoll = async () => {
-      const fetchedAccounts = await program.account.poll.all();
-      setPoll(fetchedAccounts[0]);
+  const fetchPoll = async(pollId) => {
+    if (!wallet) {
+      return alert("Connect your wallet first.");
     }
-    fetchPoll();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  
-  useEffect(() => {
-    const fetchVotes = async () => {
-      if (!wallet) return;
-      try {        
-        setVotes({
-          ethereum: parseInt(poll.account.ethereum.toString()),
-          solana: parseInt(poll.account.solana.toString()),
-          polygon: parseInt(poll.account.polygon.toString()),
-        });
-        console.log("Poll fetched.");
-        console.log(``);
-      } catch (e) {
-        setVotes({
-          ethereum: 0,
-          solana: 0,
-          polygon: 0,
-        });
-        console.log("Poll is not initialised!");
-      }
+    const poll = new PublicKey(pollId);
+    const pollAccount = await program.account.poll.fetch(poll);
+    console.log(pollAccount);
+    setVotes({
+      ethereum: parseInt(pollAccount.ethereum.toString()),
+      solana: parseInt(pollAccount.solana.toString()),
+      polygon: parseInt(pollAccount.polygon.toString()),
+    });
+    setPoll(new PublicKey(pollId));
+  }
+
+  const createNonces = async(num) => {
+    const nonceKeypairs = [];
+    for(var i = 0; i < num; i++) {
+      nonceKeypairs.push(Keypair.generate());
     }
-    fetchVotes();
-  }, [wallet, poll]);
-  
+    const tx = new Transaction();
+    tx.feePayer = nonceAuthKP.publicKey;
+    tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+    for(var j = 0; j < num; j++) {
+      tx.add(
+        SystemProgram.createAccount({
+          fromPubkey: nonceAuthKP.publicKey,
+          newAccountPubkey: nonceKeypairs[j].publicKey,
+          lamports: 0.0015 * LAMPORTS_PER_SOL,
+          space: NONCE_ACCOUNT_LENGTH,
+          programId: SystemProgram.programId,
+        }),
+        SystemProgram.nonceInitialize({
+          noncePubkey: nonceKeypairs[j].publicKey,
+          authorizedPubkey: nonceAuthKP.publicKey,
+        })
+      );
+    }
+    tx.sign(...nonceKeypairs, nonceAuthKP);
+    const sig = await sendAndConfirmRawTransaction(connection, tx.serialize({requireAllSignatures: false}));
+    console.log("Nonces initiated: ", sig);
+
+    const nonceAccounts = [];
+    for(var k = 0; k < num; k++) {
+      console.log(nonceKeypairs[k].publicKey.toString());
+      let accountInfo = await connection.getAccountInfo(nonceKeypairs[k].publicKey);
+      nonceAccounts.push(NonceAccount.fromAccountData(accountInfo.data));
+    }
+    setNoncePubKeys(nonceKeypairs.map(kp => kp.publicKey));
+    setNonces(nonceAccounts.map(acc => acc.nonce))
+    alert(`${num} nonces prepared, lets get started!`);
+  }
 
   const createPoll = async () => {
     if (!wallet) {
@@ -124,7 +170,7 @@ const Content = () => {
     }
     
     const newPoll = anchor.web3.Keypair.generate();
-  
+
     await program.methods
     .create()
     .accounts({
@@ -133,22 +179,25 @@ const Content = () => {
     })
     .signers([newPoll])
     .rpc();
-    const pollAccount = await program.account.poll.fetch(newPoll.publicKey);
-    setVotes({
-      ethereum: parseInt(pollAccount.ethereum.toString()),
-      solana: parseInt(pollAccount.solana.toString()),
-      polygon: parseInt(pollAccount.polygon.toString()),
-    });
-    setPoll(newPoll);
+    setPoll(newPoll.publicKey);
   };
 
   const vote = async (candidate) => {
+    if (nonces.length === 0) {
+      return alert("No more nonces left, create some.");
+    }
     if (!wallet) {
       return alert("Connect your wallet first.");
     } else if (!poll) {
       return alert("Create a new poll first.");
     }
-    let vote = {};
+
+    const nonce = nonces.pop();
+    const noncePubKey = noncePubKeys.pop();
+    setNonces(nonces);
+    setNoncePubKeys(noncePubKeys);
+
+    let vote = "";
     if (candidate === 0) {
       vote = "eth";
     } else if (candidate === 1) {
@@ -156,20 +205,57 @@ const Content = () => {
     } else if (candidate === 2) {
       vote = "pol";
     }
-    await program.methods
-      .vote(vote)
-      .accounts({
-        poll: poll.publicKey,
+    const ix = program.instruction.vote(vote, {
+      accounts: {
+        poll: poll,
         user: wallet.publicKey,
-      })
-      .rpc();
+      }
+    });
+    const advanceIX = SystemProgram.nonceAdvance({
+      authorizedPubkey: nonceAuthKP.publicKey,
+      noncePubkey: noncePubKey
+    })
+    const tx = new Transaction();
+    tx.add(advanceIX);
+    tx.add(ix);
+
+    tx.recentBlockhash = nonce;
+    tx.feePayer = publicKey;
+    tx.sign(nonceAuthKP);
+    const signedtx = await signTransaction(tx);
+    const ser = bs58.encode(signedtx.serialize({requireAllSignatures: false}));
+    console.log("signed!");
+
+    const row = { id: uuidv4(), transaction: ser, publicKey: publicKey, pollId: poll };
+    const { error } = await supabase
+      .from('durableTransactions')
+      .insert(row);
     
-      const pollAccount = await program.account.poll.fetch(poll.publicKey);
-      setVotes({
-        ethereum: parseInt(pollAccount.ethereum.toString()),
-        solana: parseInt(pollAccount.solana.toString()),
-        polygon: parseInt(pollAccount.polygon.toString()),
-      });
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(`inserted row to supabase: ${row}`);
+    }
+  }
+
+  const countVotes = async() => {
+    const { data } = await supabase
+      .from('durableTransactions')
+      .select('transaction')
+      .eq('pollId', poll);
+
+    for (const txObj of data) {
+      const tx = bs58.decode(txObj.transaction);
+      const sig = await sendAndConfirmRawTransaction(connection, tx);
+      console.log("Sent durable transaction: ", sig);
+    }
+    const pollAccount = await program.account.poll.fetch(poll);
+    setVotes({
+      ethereum: parseInt(pollAccount.ethereum.toString()),
+      solana: parseInt(pollAccount.solana.toString()),
+      polygon: parseInt(pollAccount.polygon.toString()),
+    });
+    console.log(votes)
   };
 
   return (
@@ -186,12 +272,29 @@ const Content = () => {
       </div>
       {publicKey ? (
         <>
-          <h1>Seahorse Poll App</h1>
-          <h3>Vote for your favorite blockchain</h3>
-          <h2>Ethereum: {votes.ethereum} | Solana: {votes.solana} | Polygon: {votes.polygon}</h2>
+          <h1>Vote for your favorite blockchain</h1>
+          <h3>Enter the Poll address or create a new one</h3>
+          <input 
+            type="text"
+            value={enteredAddress}
+            onChange={(event) => setEnteredAddress(event.target.value)} 
+          />
+          <button onClick={() => fetchPoll(enteredAddress)}>
+            Fetch Old Poll
+          </button>
           <button onClick={() => createPoll()}>
             Create New Poll
           </button>
+          <h3>Enter the number of nonces you require</h3>
+          <input 
+            type="text"
+            value={enteredNumber}
+            onChange={(event) => setEnteredNumber(event.target.value)} 
+          />
+          <button onClick={() => createNonces(enteredNumber)}>
+            Create Nonces
+          </button>
+          <p></p>
           <button onClick={() => vote(0)}>
             Vote Ethereum
           </button>
@@ -207,8 +310,12 @@ const Content = () => {
           </p>
           <h3>Poll Address</h3>
           <p>
-            {poll ? poll.publicKey.toString() : ''}
+            {poll ? poll.toString() : ''}
           </p>
+          <button onClick={() => countVotes()}>
+            Count Votes
+          </button>
+          <h2>Ethereum: {votes.ethereum} | Solana: {votes.solana} | Polygon: {votes.polygon}</h2>
         </>
       ) : (
         <p>Please connect your wallet</p>
